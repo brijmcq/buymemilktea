6f1c04d386ce69a194eecfc4a5ec8e239c8058af
Brian <brijmcq@gmail.com>
2019-09-03 18:10:24 +0800

    add github ribbon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CardComponent } from './components/card/card.component';
+import { RibbonComponent } from './components/ribbon/ribbon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    CardComponent
+    CardComponent,
+    RibbonComponent
   ],
   imports: [
     BrowserModule,
